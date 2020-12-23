b2f2603f18d4a52ab4da4b3273557a9179dfaebd
YechanLee <yechanharry@gmail.com>
2020-12-23 23:46:18 +0900

    1. toolbar 적용 및 navigation 연결

--- app/src/main/java/com/lee/oneweekonebook/ui/MainActivity.kt
+++ app/src/main/java/com/lee/oneweekonebook/ui/MainActivity.kt
@@ -3,12 +3,11 @@ package com.lee.oneweekonebook.ui
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.*
 import com.lee.oneweekonebook.R
 import com.lee.oneweekonebook.databinding.ActivityMainBinding
 
@@ -20,6 +19,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main_kotlin)
+
+        val navController = findNavController(R.id.navigation_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        findViewById<Toolbar>(R.id.toolBar_main)
+                .setupWithNavController(navController, appBarConfiguration)
 
     }
 
