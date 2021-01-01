ed2acd425828bb2ca27b5d2b13452a4c2415a3d9
YechanLee <yechanharry@gmail.com>
2021-01-01 18:13:17 +0900

    Room Database 적용

--- app/src/main/java/com/lee/oneweekonebook/database/BookDao.kt
+++ app/src/main/java/com/lee/oneweekonebook/database/BookDao.kt
@@ -1,4 +1,26 @@
 package com.lee.oneweekonebook.database
 
-class BookDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.lee.oneweekonebook.database.model.Book
+
+@Dao
+interface BookDao {
+    @Query("SELECT * FROM book")
+    fun getAll(): List<Book>
+
+    @Query("SELECT * FROM book WHERE id IN (:userIds)")
+    fun loadAllByIds(userIds: IntArray): List<Book>
+
+    @Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
+            "last_name LIKE :last LIMIT 1")
+    fun findByName(first: String, last: String): Book
+
+    @Insert
+    fun insertAll(vararg users: Book)
+
+    @Delete
+    fun delete(user: Book)
 }
--- app/src/main/java/com/lee/oneweekonebook/database/model/Book.kt
+++ app/src/main/java/com/lee/oneweekonebook/database/model/Book.kt
@@ -1,17 +1,32 @@
 package com.lee.oneweekonebook.database.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
+@Entity
 data class Book(
+        @PrimaryKey
         var id: Int = 0,
+        @ColumnInfo(name = "title")
         var title: String = "",
+        @ColumnInfo(name = "writer")
         var writer: String = "",
+        @ColumnInfo(name = "publisher")
         var publisher: String = "",
+        @ColumnInfo(name = "coverImage")
         var coverImage: String = "",
+        @ColumnInfo(name = "subTitle")
         var subTitle: String = "",
+        @ColumnInfo(name = "page")
         var page: String = "",
+        @ColumnInfo(name = "contents")
         var contents: String = "",
+        @ColumnInfo(name = "review")
         var review: String = "",
+        @ColumnInfo(name = "startDate")
         var startDate: Date = Date(),
+        @ColumnInfo(name = "endDate")
         var endDate: Date = Date(),
 )
