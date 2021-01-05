ccf71eef9b6b2e7fcec185c44ebf4eefe20372e2
YechanLee <yechanharry@gmail.com>
2021-01-05 23:56:31 +0900

    splash 화면 구현

--- app/src/main/java/com/lee/oneweekonebook/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/lee/oneweekonebook/ui/splash/SplashFragment.kt
@@ -5,11 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.lee.oneweekonebook.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        return super.onCreateView(inflater, container, savedInstanceState)
+        val binding = FragmentSplashBinding.inflate(inflater, container, false)
+
+        binding.apply {
+
+        }
+
+        return binding.root
     }
 }
