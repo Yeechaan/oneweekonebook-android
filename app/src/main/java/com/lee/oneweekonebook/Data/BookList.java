e61fdb115bc02f971715034a10c54eb0fbb49bc2
YechanLee <yechanharry@gmail.com>
2021-05-25 23:51:00 +0900

    SonarLint 검사 후 사용 안하는 Java 코드 정리

--- app/src/main/java/com/lee/oneweekonebook/Data/BookList.java
+++
@@ -1,8 +0,0 @@
-package com.lee.oneweekonebook.Data;
-
-import java.util.ArrayList;
-
-public class BookList {
-    public BookPage meta;
-    public ArrayList<BookInfo> documents = new ArrayList<BookInfo>();
-}
