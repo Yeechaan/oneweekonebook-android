652f580d02d6c8c348b82870772baa61be07f526
YechanLee <yechanharry@gmail.com>
2021-08-17 23:42:16 +0900

    Network Hilt Module refactoring

--- app/src/main/java/com/lee/oneweekonebook/di/BookApiModule.kt
+++ app/src/main/java/com/lee/oneweekonebook/di/BookApiModule.kt
@@ -1,12 +1,15 @@
 package com.lee.oneweekonebook.di
 
-import com.lee.oneweekonebook.network.BookApiQualifier
+import com.lee.oneweekonebook.network.BASE_URL_INTERPARK
 import com.lee.oneweekonebook.network.BookApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Qualifier
+
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -14,7 +17,19 @@ class BookApiModule {
 
     @BookApiQualifier
     @Provides
+    fun retrofit(): Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL_INTERPARK)
+        .build()
+
+    @BookApiQualifier
+    @Provides
     fun bookApiService(@BookApiQualifier api: Retrofit): BookApiService {
         return api.create(BookApiService::class.java)
     }
 }
+
+@Qualifier
+@MustBeDocumented
+@Retention(AnnotationRetention.RUNTIME)
+annotation class BookApiQualifier
--- app/src/main/java/com/lee/oneweekonebook/network/BookApiService.kt
+++ app/src/main/java/com/lee/oneweekonebook/network/BookApiService.kt
@@ -3,22 +3,14 @@ package com.lee.oneweekonebook.network
 import com.lee.oneweekonebook.BuildConfig
 import com.lee.oneweekonebook.ui.search.model.SearchBookResponse
 import com.lee.oneweekonebook.ui.suggest.model.RecommendBookResponse
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
-import javax.inject.Qualifier
 
 
-private const val BASE_URL_INTERPARK = "http://book.interpark.com/"
+const val BASE_URL_INTERPARK = "http://book.interpark.com/"
 const val INTERPARK_KEY = BuildConfig.BOOK_API_KEY
 const val OUTPUT_TYPE = "json"
 const val MAX_RESULT = 100
-
 
 interface BookApiService {
     @GET("api/search.api")
@@ -37,21 +29,3 @@ interface BookApiService {
         @Query("output") output: String = OUTPUT_TYPE,
     ): RecommendBookResponse
 }
-
-
-@Module
-@InstallIn(SingletonComponent::class)
-class BookRetrofitModule {
-
-    @BookApiQualifier
-    @Provides
-    fun retrofit(): Retrofit = Retrofit.Builder()
-        .addConverterFactory(GsonConverterFactory.create())
-        .baseUrl(BASE_URL_INTERPARK)
-        .build()
-}
-
-@Qualifier
-@MustBeDocumented
-@Retention(AnnotationRetention.RUNTIME)
-annotation class BookApiQualifier
